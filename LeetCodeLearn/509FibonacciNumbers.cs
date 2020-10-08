aa57725a00b252463eed1423ce127b88bd611582
luwei.jin@outlook.com <luwei.jin@outlook.com>
2020-10-08 23:10:57 +0800

    斐波那契数列计算更改，只保留两个状态

--- LeetCodeLearn/509FibonacciNumbers.cs
+++ LeetCodeLearn/509FibonacciNumbers.cs
@@ -5,17 +5,37 @@
     /// </summary>
     public class _509FibonacciNumbers
     {
-        public static int Fib(int N)
+        //用备忘录DP table 来储存已计算的状态
+        public static int Fib(int n)
         {
-            int[] dp = new int[N];
+            if (n == 0) return 0;
+            int[] dp = new int[n];
             dp[0] = dp[1] = 1;
 
-            for (int i = 2; i < N; i++)
+            for (int i = 2; i < n; i++)
             {
                 dp[i] = dp[i - 1] + dp[i - 2];
             }
 
-            return dp[N-1];
+            return dp[n - 1];
+        }
+
+        //只存储之前的两个状态,状态压缩
+        public static int BestFib(int n)
+        {
+            if (n == 2 || n == 1)
+            {
+                return 1;
+            }
+
+            int prev = 1, curr = 1;
+            for (int i = 3; i <= n; i++)
+            {
+                int sum = prev + curr;
+                prev = curr;
+                curr = sum;
+            }
+            return curr;
         }
     }
 }
