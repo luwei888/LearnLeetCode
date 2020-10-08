541fa75c5d99fb8aa1d79e9f0b7a0b69e1b76a41
luwei.jin@outlook.com <luwei.jin@outlook.com>
2020-10-08 22:55:56 +0800

    添加项目文件。

---
+++ LeetCodeLearn/509FibonacciNumbers.cs
@@ -0,0 +1,21 @@
+﻿namespace LeetCodeLearn
+{
+    /// <summary>
+    /// 斐波那契数
+    /// </summary>
+    public class _509FibonacciNumbers
+    {
+        public static int Fib(int N)
+        {
+            int[] dp = new int[N];
+            dp[0] = dp[1] = 1;
+
+            for (int i = 2; i < N; i++)
+            {
+                dp[i] = dp[i - 1] + dp[i - 2];
+            }
+
+            return dp[N-1];
+        }
+    }
+}
---
+++ LeetCodeLearn/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace LeetCodeLearn
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+
+            Console.WriteLine(_509FibonacciNumbers.Fib(25));
+
+            Console.Read();
+        }
+    }
+}
